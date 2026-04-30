96e3b40eebe7b7f91f13e4131662018a12917f07
“Nikos <nikos.handtke@uni-konstanz.de>
2026-04-30 12:35:01 +0200

    removed 67 ddetection in emojis

--- bot.js
+++ bot.js
@@ -172,7 +172,7 @@ client.on('messageCreate', async (message) => {
   const containsMogus = mogusWords.some(word =>
     lowerContent.includes(word)
   );
-  
+  const cleanContent = message.content.replace(/<a?:\w+:\d+>/g, '');
   const contains67 = /6.*7|six.*seven|zes.*zeven|six.*sept|sechs.*sieben/i.test(message.content);
   
   // Check if message contains job words
